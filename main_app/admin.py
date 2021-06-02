b4728777506d94d47495339aafdf82ca949e05b5
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-06-02 14:28:42 -0700

    made migrations for trail and community hike models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Trail, CommunityHike
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Trail)
+admin.site.register(CommunityHike)
--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Model
 
+
 # Create your models here.
 
 
@@ -11,3 +12,24 @@ class Profile(Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+
+
+class Trail(Model):
+
+    trail_name = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+    longitude = models.DecimalField(max_digits=15, decimal_places=10)
+    latitude = models.DecimalField(max_digits=15, decimal_places=10)
+    length = models.CharField(max_length=50)
+    difficulty = models.CharField(max_length=50)
+    favorited = models.ManyToManyField(Profile)
+
+
+class CommunityHike(Model):
+
+    creator = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="community_hikes")
+    trail = models.ForeignKey(Trail, on_delete=models.CASCADE, related_name="community_hikes")
+    date = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+    attendees = models.ManyToManyField(Profile)
