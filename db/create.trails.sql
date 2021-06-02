310f8d7a9bf6db7e435c7f263e8b0c1450642d28
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-06-02 15:33:30 -0700

    starting creating seed data for trails & mapbox map now displays on pages

---
+++ db/create.trails.sql
@@ -0,0 +1,10 @@
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty, favorited) 
+    VALUES
+        ('Marquam Trail to Council Crest Hike Trail', 'Dog Friendly', 'near Portland, Oregon in Council Crest City Park', '-122.68712', '45.49107', '3.1 miles', 'moderate', '8')
+
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty, favorited) 
+    VALUES
+        ('White Pine, Wildwood, Mac and Walnut Trail Loop', 'Hiking', 'near Portland, Oregon in Washington State Park', '38.0838', '90.6833', '5.1 miles', 'moderate', '7')
+
+        
