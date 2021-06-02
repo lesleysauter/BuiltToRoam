efc51855860776e12c250594ac637013d090411b
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-06-02 16:27:31 -0700

    minor css changes to nav bar and signup.html

--- db/create.trails.sql
+++ db/create.trails.sql
@@ -1,10 +1,21 @@
 INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty, favorited) 
     VALUES
-        ('Marquam Trail to Council Crest Hike Trail', 'Dog Friendly', 'near Portland, Oregon in Council Crest City Park', '-122.68712', '45.49107', '3.1 miles', 'moderate', '8')
+        ('Marquam Trail to Council Crest Hike Trail', 'Dog Friendly', 'near Portland, Oregon in Council Crest City Park', '-122.68712', '45.49107', '3.1 miles', 'moderate', '8');
 
 
 INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty, favorited) 
     VALUES
-        ('White Pine, Wildwood, Mac and Walnut Trail Loop', 'Hiking', 'near Portland, Oregon in Washington State Park', '38.0838', '90.6833', '5.1 miles', 'moderate', '7')
+        ('White Pine, Wildwood, Mac and Walnut Trail Loop', 'Hiking', 'near Portland, Oregon in Washington State Park', '90.6833', '38.0838', '5.1 miles', 'moderate', '7');
 
-        
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty, favorited) 
+    VALUES
+        ('Burnt Lake Trail', 'Views', 'near Rhododendron, Oregon in Mount Hood Wilderness', '-121.80236', '45.35090', '7.7 miles', 'moderate', '7');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty, favorited) 
+    VALUES
+        ('Top Spur and Timberline Trail Loop', 'Dog Friendly', 'near Government Camp, Oregon in Mount Hood Wilderness', '-121.78589', '45.40775', '2.1 miles', 'easy', '7');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty, favorited) 
+    VALUES
+        ('PCT: OR Section G - Highway 35 to Cascade Locks (Bridge of the Gods)', 'Camping', 'near Mount Hood Parkdale, Oregon in Mount Hood Wilderness', '-121.68491', '45.282.38', '52.4 miles', 'hard', '7');
+
