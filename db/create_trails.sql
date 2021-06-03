4b486d9a135636655d793bd97afa3aacb50d40ab
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-06-03 12:33:30 -0700

    added more seed data in sql file

---
+++ db/create_trails.sql
@@ -0,0 +1,90 @@
+TRUNCATE TABLE main_app_trail CASCADE;
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Marquam Trail to Council Crest Hike Trail', 'Dog Friendly', 'near Portland, Oregon in Council Crest City Park', '-122.68712', '45.49107', '3.1 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('White Pine, Wildwood, Mac and Walnut Trail Loop', 'Hiking', 'near Portland, Oregon in Washington State Park', '-90.6833', '38.0838', '5.1 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Burnt Lake Trail', 'Views', 'near Rhododendron, Oregon in Mount Hood Wilderness', '-121.80236', '45.35090', '7.7 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Top Spur and Timberline Trail Loop', 'Dog Friendly', 'near Government Camp, Oregon in Mount Hood Wilderness', '-121.78589', '45.40775', '2.1 miles', 'easy');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('PCT: OR Section G - Highway 35 to Cascade Locks (Bridge of the Gods)', 'Camping', 'near Mount Hood Parkdale, Oregon in Mount Hood Wilderness', '-121.68491', '45.28238', '52.4 miles', 'hard');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Snoopers Rock Trail', 'River', 'near Chattanooga, Tennessee in Prentice Cooper State Forest', '-85.41915', '35.13268', '5.9 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Middle Creek Falls', 'Rock Climbing', 'near Chattanooga, Tennessee', '-124.0109', '43.2151', '1.5 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Rose Valley Falls', 'Waterfall', 'near Ojai, California in Los Padres National Forest', '-119.18309', '34.53187', '1.1 miles', 'easy');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Sespe River Trail To Sespe Hot Springs', 'Camping', 'near Ojai, California in Sespe Wilderness', '-119.16603', '34.56001', '17 miles', 'hard');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Panorama Point from Paradise Inn', 'Views', 'near Paradise Inn, Washington in Mount Rainier National Park', '-121.73412', '46.78669', '4.1 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Skyline Trail Loop', 'River', 'near Paradise Inn, Washington in Mount Rainier National Park', '-121.7362', '46.7866', '5.6 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Avalanche Peak Trail', 'Rock Climbing', 'near Yellowstone National Park, Wyoming', '-110.1403', '44.48970', '4.7 miles', 'hard');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Pate Hollow Short Loop', 'Dog Friendly', 'near Smithville, Indiana in Hoosier National Forest', '-86.42441', '39.09247', '3 miles', 'easy');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Lost Mine Trail', 'Hiking', 'near Big Bend National Park, Texas', '-103.28705', '29.27458', '4.8 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('The Window Trail', 'Views', 'near Terlingua, Texas in Big Bend National Park', '-103.30106', '29.27021', '5.2 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Multnomah Falls Trail', 'Waterfall', 'near Bridal Veil, Oregon in Columbia River Gorge National Scenic Area', '-122.11845', '45.57882', '2.4 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Dog Mountain Trail', 'River', 'near Bingen, Washington in Columbia River Gorge National Scenic Area', '-121.70815', '45.69934', '6.5 miles', 'hard');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Avalanche Lake via the Trail of the Cedars', 'Views', 'near Lake McDonald, Montana in Glacier National Park', '-113.81956', '48.67945', '5.9 miles', 'moderate');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('The Garden Wall', 'Camping', 'near West Glacier, Montana in Glacier National Park', '-113.71774', '48.69646', '14.7 miles', 'hard');
+
+INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+    VALUES
+        ('Lava Lake (Cascade Creek) Trail', 'Dog Friendly', 'near Gallatin Gateway, Montana in Custer Gallatin National Forest', '-111.22448', '45.40643', '6 miles', 'moderate');
+
+
+
+
+
+-- INSERT INTO main_app_trail(trail_name, category, location, longitude, latitude, length, difficulty) 
+--     VALUES
+--         ('', '', '', '', '', '', '');
+
