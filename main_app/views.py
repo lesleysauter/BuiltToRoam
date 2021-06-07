6b718dec8af4a4e6403cd3c86cb7d5709802aeab
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-06-07 15:27:19 -0700

    added markers to mapbox

--- main_app/views.py
+++ main_app/views.py
@@ -110,6 +110,12 @@ class CreateCommunityEvent(CreateView):
         form.instance.creator = self.request.user.profile
         form.instance.trail = Trail.objects.get(pk=self.kwargs["trail_pk"])
         return super(CreateCommunityEvent, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["trail"] = Trail.objects.get(pk=self.kwargs["trail_pk"])
+        print(context)
+        return context
     
     def get_success_url(self):
         return reverse('viewevent', kwargs={'pk': self.object.pk})
