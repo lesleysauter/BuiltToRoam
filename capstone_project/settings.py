d9dae11b8c8ce49ed97ab86f1491d731fb04dff4
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-06-08 22:09:42 -0700

    deployment

--- capstone_project/settings.py
+++ capstone_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_REDIRECT_URL = '/'
 
 AUTHENTICATION_BACKENDS = ["main_app.EmailBackend.EmailBackend"]
+
+django_on_heroku.settings(locals())
